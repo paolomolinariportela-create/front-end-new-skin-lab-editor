54b50ae5f4c03ee1dc1a12b264e2946157f12d53
paolomolinariportela-create <paolomolinariportela@gmail.com>
2026-02-03 11:42:57 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'front-end-new-skin-lab-editor-production.up.railway.app'
+    ],
+    host: '0.0.0.0', // Permite conexões externas
+  },
+  preview: {
+    allowedHosts: [
+      'front-end-new-skin-lab-editor-production.up.railway.app'
+    ],
+    host: '0.0.0.0',
+    port: 4173 // Porta padrão do vite preview
+  }
 })
